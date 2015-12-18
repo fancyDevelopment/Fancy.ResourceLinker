aa7dd8a4b70d4355867c05865127dbc3d16f5ba1
Daniel Murrmann <daniel.murrmann@live.de>
2015-12-18 20:57:00 +0100

    Added link stratgy interface and link controller base class

---
+++ src/Fancy.ResourceLinker/LinkController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Fancy.ResourceLinker
+{
+    /// <summary>
+    /// Base class of a controller which links its resources to related resources
+    /// </summary>
+    public class LinkController : Controller
+    {
+    }
+}
