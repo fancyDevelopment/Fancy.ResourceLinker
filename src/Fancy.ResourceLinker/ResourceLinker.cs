e84c161f34e843718cdca6c37174924169b64510
Daniel Murrmann <daniel.murrmann@live.de>
2015-12-21 20:13:16 +0100

    Added implementation skeleton for resource linker.

---
+++ src/Fancy.ResourceLinker/ResourceLinker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Fancy.ResourceLinker.Models;
+
+namespace Fancy.ResourceLinker
+{
+    /// <summary>
+    /// Implements the <see cref="IResourceLinker"/> interface.
+    /// </summary>
+    public class ResourceLinker : IResourceLinker
+    {
+        /// <summary>
+        /// Adds links to a resource using a link strategy.
+        /// </summary>
+        /// <typeparam name="TResource">The type of resource to add links to.</typeparam>
+        /// <param name="resource">The resource to add links to.</param>
+        public void AddLinks<TResource>(TResource resource) where TResource : ResourceBase
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds links to a collection of resources using a link strategy.
+        /// </summary>
+        /// <typeparam name="TResource">The type of resource to add links to.</typeparam>
+        /// <param name="resources">The resources to add links to.</param>
+        public void AddLinks<TResource>(IEnumerable<TResource> resources) where TResource : ResourceBase
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Fancy.ResourceLinker/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Fancy.ResourceLinker
+{
+    /// <summary>
+    /// Extension class with helper to easily register resource linker to ioc container.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the resource linker to the ioc container.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to add the resource linker to.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddResourceLinker(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(typeof (IResourceLinker), typeof (ResourceLinker));
+            return serviceCollection;
+        }
+    }
+}
