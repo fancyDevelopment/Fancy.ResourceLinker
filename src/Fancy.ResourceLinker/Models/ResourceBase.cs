1034fa376f6aed62b6e3a4575505733c7c315581
Daniel Murrmann <daniel.murrmann@live.de>
2018-05-26 23:49:03 +0200

    Extened ResourceBase to be able to be extensible with additional properties.

--- src/Fancy.ResourceLinker/Models/ResourceBase.cs
+++ src/Fancy.ResourceLinker/Models/ResourceBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Dynamic;
 using Newtonsoft.Json;
 
 namespace Fancy.ResourceLinker.Models
@@ -6,8 +7,13 @@ namespace Fancy.ResourceLinker.Models
     /// <summary>
     /// Base class of a resource which can be linked to other resources.
     /// </summary>
-    public class ResourceBase
+    public class ResourceBase : DynamicObject
     {
+        /// <summary>
+        /// The dynamic properties of this resource.
+        /// </summary>
+        Dictionary<string, object> _dynamicProperties = new Dictionary<string, object>();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ResourceBase"/> class.
         /// </summary>
@@ -55,5 +61,48 @@ namespace Fancy.ResourceLinker.Models
         /// </value>
         [JsonProperty("_actions")]
         public Dictionary<string, ResourceAction> Actions { get; }
+
+        /// <summary>
+        /// Tries to get a dynamic member.
+        /// </summary>
+        /// <param name="binder">The binder.</param>
+        /// <param name="result">The result.</param>
+        /// <returns>true if the member could be retrieved; otherwise, false.</returns>
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (_dynamicProperties.ContainsKey(binder.Name))
+            {
+                result = _dynamicProperties[binder.Name];
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Tries to set a dynamic member.
+        /// </summary>
+        /// <param name="binder">The binder.</param>
+        /// <param name="value">the value to set.</param>
+        /// <returns>
+        /// true if the member could be set; otherwise, false.
+        /// </returns>
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _dynamicProperties[binder.Name] = value;
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the enumeration of all dynamic member names.
+        /// </summary>
+        /// <returns>
+        /// A sequence that contains dynamic member names.
+        /// </returns>
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return _dynamicProperties.Keys;
+        }
     }
 }
