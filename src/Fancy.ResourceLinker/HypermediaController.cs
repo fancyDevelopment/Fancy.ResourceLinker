92b9255f542f2d5498717ee78f7ec5054fc7c6d5
Daniel Murrmann <daniel.murrmann@live.de>
2021-01-02 12:52:01 +0100

    Implemented HypermediaController as a base class for controllers delivering hypermedia documents

---
+++ src/Fancy.ResourceLinker/HypermediaController.cs
@@ -0,0 +1,21 @@
+﻿using Fancy.ResourceLinker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Fancy.ResourceLinker
+{
+    public class HypermediaController : ControllerBase
+    {
+        public virtual IActionResult Hypermedia<TResource>(TResource content) where TResource : ResourceBase
+        {
+            this.LinkResource(content);
+            return new ObjectResult(content);
+        }
+
+        public virtual IActionResult Hypermedia<TResource>(IEnumerable<TResource> content) where TResource : ResourceBase
+        {
+            this.LinkResources(content);
+            return new ObjectResult(content);
+        }
+    }
+}
