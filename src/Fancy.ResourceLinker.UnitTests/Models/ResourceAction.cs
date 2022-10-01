c2c8ae88f9faaa70c07f982a070630ed4eb790fe
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2022-10-01 13:41:35 +0200

    Added test for ResourceAction

---
+++ src/Fancy.ResourceLinker.UnitTests/Models/ResourceAction.cs
@@ -0,0 +1,28 @@
+using Fancy.ResourceLinker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Fancy.ResourceLinker.UnitTests.Models
+{
+    [TestClass]
+    public class ResourceActionTests
+    {
+        [TestMethod]
+        public void Constructor_WithValidArguments_CreatesInstance()
+        {
+            var target = new ResourceAction("PUT", "http://foo.bar/baz");
+            Assert.IsNotNull(target);
+            target = new ResourceAction("POST", "http://foo.bar/baz");
+            Assert.IsNotNull(target);
+            target = new ResourceAction("DELETE", "http://foo.bar/baz");
+            Assert.IsNotNull(target);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WithGETVerb_ThrowsException()
+        {
+            var target = new ResourceAction("GET", "http://foo.bar/baz");
+        }
+    }
+}
