b1cd48351581d1e7f9adc65f71ae3526fadecfd6
Daniel Murrmann <daniel.murrmann@live.de>
2024-04-25 19:09:26 +0200

    Documentation update

--- src/Fancy.ResourceLinker.Gateway/Authentication/GatewayAuthentication.cs
+++ src/Fancy.ResourceLinker.Gateway/Authentication/GatewayAuthentication.cs
@@ -1,5 +1,4 @@
 ﻿using Fancy.ResourceLinker.Gateway;
-using Fancy.ResourceLinker.Gateway.Common;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
